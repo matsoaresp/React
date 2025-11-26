ea1307e3e37ab7f13cc1e4e4cfd19fb07d191bf0
Mateus Prado <maateussp@gmail.com>
2025-11-26 13:14:23 -0300

    Learn about hooks

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,16 @@
 
 import { count } from 'console';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useContext, useEffect, useRef } from 'react';
 import { Link, useHistory } from 'react-router-dom'
+import { UsuarioLogadoContext } from '../../shared/context';
+import { useUsuarioLogado } from '../../shared/hooks';
 export const Dashboard = () => {
 
     const counterRef = useRef({ counter: 0 });
     const history = useHistory();
+    const usuarioLogadoContext = useContext(UsuarioLogadoContext);
 
+    const {nomeDoUsuario,logout} = useUsuarioLogado(); 
 
     if (counterRef.current.counter < 0) {
         counterRef.current.counter = 0
@@ -21,11 +25,16 @@ export const Dashboard = () => {
  rounded p-4 p-6 w-80 flex flex-col">
                 <nav className='flex justify-center'>
                     <p>Dashboard</p>
+
                 </nav>
+                <div className='flex justify-center'>
+                    <p>{usuarioLogadoContext.nomeUsuario}</p>
+                    </div>
 
                 <p>Contador:  {counterRef.current.counter}</p>
                 <div className='flex gap-4 justify-center'>
                     <Link className='bg-red-500 text-white px-4 py-2 rounded' to="/entrar" onClick={() => console.log(counterRef.current.counter--)}>-</Link>
+                    <Link className='bg-red-500 text-white px-4 py-2 rounded' to="/entrar" onClick={() => console.log(counterRef.current.counter = 0)}>Zerar</Link>
                     <Link className='bg-green-500 text-white px-4 py-2 rounded' to= "/entrar" onClick={() => console.log(counterRef.current.counter++)}>+</Link>
                 </div>
 
